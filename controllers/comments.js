1f5983f407f852a9c61790b92ff8038bc2283801
Steven <steven_moxon123@hotmail.co.uk>
2017-11-01 12:40:53 +0000

    [addComment] add controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,6 +1,6 @@
 const { Comments } = require('../models/models');
 exports.getCommentsByArticle = (req, res) => {
-    Comments.find({belongs_to : req.params.article_id})
+    Comments.find({ belongs_to: req.params.article_id })
         .then((comments) => {
             if (!comments) {
                 return resizeBy.status(404).json({ message: 'Article not found' });
@@ -12,3 +12,18 @@ exports.getCommentsByArticle = (req, res) => {
             rej.status(500);
         });
 };
+
+exports.postNewComment = ((req, res) => {
+    const comment = new Comments({
+        body: req.body.body,
+        belongs_to: req.params.article_id
+    });
+    comment.save()
+        .then((comment) => {
+            res.status(201).json(comment);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+
+});
