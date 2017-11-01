a447b4f6e873306de45185b26f08ff69c5e46bd2
Steven <steven_moxon123@hotmail.co.uk>
2017-11-01 12:39:58 +0000

    [server.js] add body parser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
+const bodyParser = require('body-parser');
 const config = require('./config');
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
@@ -11,16 +12,18 @@ const routes = require('./routers/router');
 
 mongoose.connect(db, () => {
     console.log('connected to db');
-  });
+});
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.status(200).send('All good!');
-  });
-  
-  app.use('/api', routes);
-  
-  app.listen(PORT, function () {
+});
+
+app.use('/api', routes);
+
+app.listen(PORT, function () {
     console.log(`listening on port ${PORT}`);
-  });
-  
-  module.exports = app;
+});
+
+module.exports = app;
