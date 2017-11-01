64ca392b61481d7b7e8cba7e2bb1ca0c618260f2
Steven <steven_moxon123@hotmail.co.uk>
2017-11-01 14:07:22 +0000

    add config for deployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     DB: {
       test: 'mongodb://localhost/northcoders-news-api-test',
-      dev: 'mongodb://localhost/northcoders-news-api'
+      dev: 'mongodb://user:password@ds243805.mlab.com:43805/northcodersnewsdb'
     },
     PORT: {
       test: 3090,
